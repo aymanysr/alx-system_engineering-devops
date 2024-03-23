3e4da7d6d0485876c51ba00d897c56e90f05d3c6
aimaneysr <aimaneyousr@gmail.com>
2024-03-23 02:54:36 +0000

    Add Puppet manifest to create a file named 'school' in /tmp directory

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+#this code creates a file named school inside the /tmp directory
+file { '/tmp/school':
+  ensure  =>  file,
+  content =>  'I love Puppet',
+  mode    =>  '0744',
+  owner   =>  'www-data',
+  group   =>  'www-datat',
+}
