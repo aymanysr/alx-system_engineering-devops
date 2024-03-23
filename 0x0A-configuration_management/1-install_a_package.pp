52524976c08e8a24180c89f46e76ae00651f3920
aimaneysr <aimaneyousr@gmail.com>
2024-03-23 02:54:57 +0000

    Add installation of Flask and Werkzeug with specific versions via pip3

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,10 @@
+# This code will install Flask and Werkzeug via pip3 with specific versions
+package { 'flask':
+    ensure  =>  '2.1.0',
+    provider  =>  'pip3',
+}
+
+package { 'werkzeug':
+    ensure  =>  '2.0.2', # adjust this version as needed
+    provider  =>  'pip3',
+}
