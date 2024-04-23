419e120be302e512a15aaa1adca44cf5b92f9706
aimane yousr <aimaneyousr@gmail.com>
2024-04-23 15:34:19 +0100

    fix to the script to gather data from an API and display TODO list progress

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,31 +15,26 @@ if __name__ == "__main__":
 
     employee_id = sys.argv[1]
 
-    # Fetch employee data
     base_url = "https://jsonplaceholder.typicode.com"
+
     user_url = f"{base_url}/users/{employee_id}"
-    todo_url = f"{base_url}/todos?userId={employee_id}"
-
     user_response = requests.get(user_url)
-    todo_response = requests.get(todo_url)
-
-    if user_response.status_code != 200 or todo_response.status_code != 200:
-        print("Failed to retrieve data")
-        sys.exit(1)
-
     user_data = user_response.json()
+
+    parameters = {"userId": employee_id}
+
+    todo_url = f"{base_url}/todos/"
+    todo_response = requests.get(todo_url, params=parameters)
     todo_data = todo_response.json()
 
-    employee_name = user_data["name"]
-    all_tasks = len(todo_data)
+    completed = []
 
-    completed_tasks = [task for task in todo_data if task["completed"]]
-    completed_count = len(completed_tasks)
+    for todo in todo_data:
+        if todo.get("completed") is True:
+            completed.append(todo.get("title"))
 
-    print(
-        f"Employee {employee_name} is done with tasks"
-        f"({completed_count}/{all_tasks}):"
-    )
+    print(f"Employee {user_data.get('name')} is done with tasks"
+          f"({len(completed)}/{len(todo_data)}):")
 
-    for task in completed_tasks:
-        print(f"\t {task['title']}")
+    for complete in completed:
+        print("\t {}".format(complete))
